8f3bc380891a8b66894a71f376fe110dbc735273
Subhi Qudsi <s.qudsi@sitech.me>
2025-05-08 20:06:56 +0300

    dist

---
+++ .output/public/_nuxt/DHth_Dhn.js
@@ -0,0 +1,1 @@
+import{A as a,h as n,o as s,w as f,y as p,_}from"./CkMRiQpX.js";const l=a({__name:"ProseA",props:{href:{type:String,default:""},target:{type:String,default:void 0,required:!1}},setup(t){const e=t;return(r,c)=>{const o=_;return s(),n(o,{href:e.href,target:e.target},{default:f(()=>[p(r.$slots,"default")]),_:3},8,["href","target"])}}});export{l as default};
